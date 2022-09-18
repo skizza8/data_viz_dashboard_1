f5f235f516ae14e2d584e26c05b4f46c014f97ba
Kizza Samuel <samuel.kizza@finca.org>
2022-09-18 12:17:29 +0300

    first commit

--- app.js
+++ app.js
@@ -1,260 +1,377 @@
 const datas = [
-    { language : "A", value : 47 },
-    { language : "B", value : 81 },
-    { language : "C", value : 33 },
-    { language : "D", value : 54.21 },
-    { language : "E", value : 41.78 },
-    { language : "F", value : 41.78 },
-    { language : "G", value : 41.78 },
-    { language : "H", value : 41.78 },
-    { language : "I", value : 41.78 },
+    { language: "A", value: 47 },
+    { language: "B", value: 81 },
+    { language: "C", value: 33 },
+    { language: "D", value: 54.21 },
+    { language: "E", value: 41.78 },
+    { language: "F", value: 41.78 },
+    { language: "G", value: 41.78 },
+    { language: "H", value: 41.78 },
+    { language: "I", value: 41.78 },
 ]
 const area = document.querySelector("#bar")
+const area_scatter = document.querySelector("#scatter")
 const body = document.querySelector('body')
-body.onresize = function(){
+body.onresize = function () {
     const barSVG = document.querySelector("#svg-bar")
-    if(barSVG != null){
+    if (barSVG != null) {
         barSVG.remove()
         drawBarChart()
     }
-    const lineSVG = document.querySelector('#svg-line')
-    if(lineSVG != null){
-        lineSVG.remove()
-        drawLineChart()
-    }    
-    const pieSVG =  document.querySelector('#svg-pie')
-    if(pieSVG != null){
+    const scatterSVG = document.querySelector('#svg-scatter')
+    if (scatterSVG != null) {
+        scatterSVG.remove()
+        drawScatterChart()
+    }
+    const pieSVG = document.querySelector('#svg-pie')
+    if (pieSVG != null) {
         pieSVG.remove()
         drawPieChart()
     }
 }
 drawBarChart()
-function drawBarChart(){
+function drawBarChart() {
 
-    const   margin  = {y : 40, x : 60},
-            width   = area.offsetWidth - (2 * margin.x),
-            height  = area.offsetHeight - (2 * margin.y);
-    const svg = d3.select(area).append("svg")
-    svg.attr("width",width + (2 * margin.x))
-    .attr("height",height + (2 * margin.y))
-    .attr('id','svg-bar')
-    const chart = svg.append('g')
-    .attr('transform',`translate(${margin.x},${margin.y})`)
+    //d3.csv("data/votes_per_candidate.csv", function (tdata) {
+    d3.csv("data/votes_per_candidate.csv").then(function (tdata) {
+        console.log(tdata);
+        /*if (error) {
+            throw error;
+        }*/
 
-    const yScale = d3.scaleLinear()
-    .range([height,0])
-    .domain([0, 100])
+        //xScale.domain(data.map(function (d) { return d.candidate; }));
+        //yScale.domain([d3.min(data, function (d) { return d.votes; }), d3.max(data, function (d) { return d.votes; })]);
 
-    chart.append('g')
-    .attr('class','axis')
-    .call(d3.axisLeft(yScale))
+        const margin = { y: 40, x: 100 },
+            width = area.offsetWidth - (2 * margin.x),
+            height = area.offsetHeight - (2 * margin.y);
+        const svg = d3.select(area).append("svg")
+        svg.attr("width", width + (2 * margin.x))
+            .attr("height", height + (2 * margin.y))
+            .attr('id', 'svg-bar')
+        const chart = svg.append('g')
+            .attr('transform', `translate(${margin.x},${margin.y})`)
 
-    const xScale = d3.scaleBand()
-    .range([0, width])
-    .domain(datas.map((d) => d.language))
-    .padding(0.2)
+        const yScale = d3.scaleLinear()
+            .range([height, 0])
+            //.domain([0, 100])
+            .domain([0, d3.max(tdata, (d) => (d.percent_votes))])
 
-    chart.append('g')
-    .attr('class','axis')
-    .attr('transform',`translate(0,${height})`)
-    .call(d3.axisBottom(xScale))
+        chart.append('g')
+            .attr('class', 'axis')
+            .call(d3.axisLeft(yScale))
 
+        const xScale = d3.scaleBand()
+            .range([0, width])
+            .domain(tdata.map((d) => d.candidate))
+            //.domain(d3.map(tdata,function (d) { return d.candidate; }))
+            .padding(0.2)
 
-    
-    //Draw grid
-    chart.append('g')
-    .attr('class', 'grid-hline')
-    .call(d3.axisLeft().scale(yScale).tickSize(-width,0,0).tickFormat(''))
-    //End Draw grid
-    
-
-    //Top Title
-    svg.append('text')
-    .attr('x', (width / 2) + margin.x)
-    .attr('y', margin.y / 2)
-    .attr('class','title')
-    .attr('text-anchor','middle')
-    .text('Bar chart')
-    //End Top Title
-
-    //x Axis Title
-    svg.append('text')
-    .attr('x', (width / 2) + margin.x)
-    .attr('y', (margin.y * 2))
-    .attr('transform',`translate(0,${height - (margin.y / 4) })`)
-    .attr('class','title')
-    .text('x axis title')
-    //End x axis title
-
-    //y Axis Title
-    svg.append('text')
-    .attr('class', 'title')
-    .attr('x',-(height/2) - margin.y)
-    .attr('y', margin.x / 2.4)
-    .attr('transform', 'rotate(-90)')
-    .text('y axis title')
-    //End y Axis Title
+        chart.append('g')
+            .attr('class', 'axis')
+            .attr('transform', `translate(0,${height})`)
+            .call(d3.axisBottom(xScale))
 
 
-    chart.selectAll()
-    .data(datas)
-    .enter()
-        .append('rect')
-        .attr('class','bar')
-        .attr('x', (d) => xScale(d.language))
-        .attr('y', (d) => yScale(d.value))
-        .attr('height', (d) => height - yScale(d.value))
-        .attr('width', xScale.bandwidth())
-        .on('mouseenter', function(){
-            d3.select(this).attr('class','hover-bar')
-        })
-        .on('mouseleave', function(){
-            d3.select(this).attr('class','bar')
-        })
-    
+
+        //Draw grid
+        chart.append('g')
+            .attr('class', 'grid-hline')
+            .call(d3.axisLeft().scale(yScale).tickSize(-width, 0, 0).tickFormat(''))
+        //End Draw grid
+
+
+        //Top Title
+        svg.append('text')
+            .attr('x', (width / 2) + margin.x)
+            .attr('y', margin.y / 2)
+            .attr('class', 'title')
+            .attr('text-anchor', 'middle')
+            .text('% Votes Per Candidate')
+        //End Top Title
+
+        //x Axis Title
+        svg.append('text')
+            .attr('x', (width / 2) + margin.x)
+            .attr('y', (margin.y * 2))
+            .attr('transform', `translate(0,${height - (margin.y / 4)})`)
+            .attr('class', 'title')
+            .text('Candidate')
+        //End x axis title
+
+        //y Axis Title
+        svg.append('text')
+            .attr('class', 'title')
+            .attr('x', -(height / 2) - margin.y)
+            .attr('y', margin.x / 2.4)
+            .attr('transform', 'rotate(-90)')
+            .text('% Votes')
+        //End y Axis Title
+
+
+        chart.selectAll()
+            .data(tdata)
+            .enter().append('rect')
+            .attr('class', 'bar')
+            .attr('x', (d) => xScale(d.candidate))
+            .attr('y', (d) => yScale(d.percent_votes))
+            .attr('height', (d) => height - yScale(d.percent_votes))
+            .attr('width', xScale.bandwidth())
+            .style('fill', (d) => d.party_color)
+            .on('mouseenter', function () {
+                d3.select(this).attr('class', 'hover-bar')
+
+            })
+            .on('mouseleave', function () {
+                d3.select(this).attr('class', 'bar')
+            })
+
+
+
+
+    });
 }
 
+
+drawScatterChart()
+function drawScatterChart() {
+
+
+    // set the dimensions and margins of the graph
+    var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+        width = area_scatter.offsetWidth - margin.left - margin.right,
+        height = area_scatter.offsetHeight - margin.top - margin.bottom;
+    
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#scatter")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+    
+
+    //d3.csv("data/overall_stats_2.csv", function (tdata){
+    d3.csv("data/overall_stats_2.csv").then(function (data) {
+        
+        console.log(data);
+        data.forEach(function(d) {
+            d.per_turnup = d.per_turnup;
+            d.invalid_votes = parseInt(d.invalid_votes);
+        });
+
+        var max_invalid_votes = d3.max(data, function(d) { return d.invalid_votes; });
+        var min_invalid_votes = d3.min(data, function(d) { return d.invalid_votes; });
+
+        var max_turnup = d3.max(data, function(d) { return d.per_turnup; });
+        var min_turnup = d3.min(data, function(d) { return d.per_turnup; });
+
+        // Add X axis
+        var x = d3.scaleLinear()
+            .domain([min_invalid_votes, max_invalid_votes])
+            .range([0, width]);
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        // Add Y axis
+        var y = d3.scaleLinear()
+            .domain([min_turnup, max_turnup])
+            .range([height, 0]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        //Top Title
+        svg.append('text')
+            .attr('x', (width / 2) + margin.left + margin.right)
+            .attr('y', margin.top / 2)
+            .attr('class', 'title')
+            .attr('text-anchor', 'middle')
+            .text('% Voter turnout Vs Invalid votes')
+        //End Top Title
+
+        //x Axis Title
+        svg.append('text')
+            .attr('x', (width / 2) + margin.left + margin.right)
+            .attr('y', (margin.top * 2.5))
+            .attr('transform', `translate(0,${height - (margin.top / 4)})`)
+            .attr('class', 'title')
+            .text('Invalid votes')
+        //End x axis title
+
+        //y Axis Title
+        svg.append('text')
+            .attr('class', 'title')
+            .attr('x', -(height / 2) - margin.top)
+            .attr('y', (margin.right))
+            .attr('transform', 'rotate(-90)')
+            .text('% Voter turnout')
+        //End y Axis Title
+
+        // Add dots
+        svg.append('g')
+            .selectAll("dot")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cy", function (d) { return y(d.per_turnup); })
+            .attr("cx", function (d) { return x(d.invalid_votes); })
+            .attr("r", 1.5)
+            .style("fill", "#69b3a2")
+
+
+    });
+
+}
+
+
+
+
+/*
 drawLineChart()
-function drawLineChart(){
+function drawLineChart() {
     const area = document.querySelector('#line')
-    const   margin  = {y : 40, x : 60},
-    width   = area.offsetWidth - (2 * margin.x),
-    height  = area.offsetHeight - (2 * margin.y);
+    const margin = { y: 40, x: 60 },
+        width = area.offsetWidth - (2 * margin.x),
+        height = area.offsetHeight - (2 * margin.y);
     const svg = d3.select(area).append("svg")
-    svg.attr("width",width + (2 * margin.x))
-    .attr("height",height + (2 * margin.y))
-    .attr('id','svg-line')
+    svg.attr("width", width + (2 * margin.x))
+        .attr("height", height + (2 * margin.y))
+        .attr('id', 'svg-line')
     const chart = svg.append('g')
-    .attr('transform',`translate(${margin.x},${margin.y})`)
+        .attr('transform', `translate(${margin.x},${margin.y})`)
 
     const yScale = d3.scaleLinear()
-    .range([height,0])
-    .domain([0, 100])
+        .range([height, 0])
+        .domain([0, 100])
 
     chart.append('g')
-    .attr('class','axis')
-    .call(d3.axisLeft(yScale))
+        .attr('class', 'axis')
+        .call(d3.axisLeft(yScale))
 
     const xScale = d3.scaleBand()
-    .range([0, width])
-    .domain(datas.map((d) => d.language))
-    .padding(0.2)
+        .range([0, width])
+        .domain(datas.map((d) => d.language))
+        .padding(0.2)
 
     chart.append('g')
-    .attr('class','axis')
-    .attr('transform',`translate(0,${height})`)
-    .call(d3.axisBottom(xScale))
+        .attr('class', 'axis')
+        .attr('transform', `translate(0,${height})`)
+        .call(d3.axisBottom(xScale))
 
     //Draw grid
     chart.append('g')
-    .attr('class', 'grid-hline')
-    .call(d3.axisLeft().scale(yScale).tickSize(-width,0,0).tickFormat(''))
+        .attr('class', 'grid-hline')
+        .call(d3.axisLeft().scale(yScale).tickSize(-width, 0, 0).tickFormat(''))
     //End Draw grid
 
     //Top Title
     svg.append('text')
-    .attr('x', (width / 2) + margin.x)
-    .attr('y', margin.y / 2)
-    .attr('class','title')
-    .attr('text-anchor','middle')
-    .text('Line chart')
+        .attr('x', (width / 2) + margin.x)
+        .attr('y', margin.y / 2)
+        .attr('class', 'title')
+        .attr('text-anchor', 'middle')
+        .text('Line chart')
     //End Top Title
 
     //x Axis Title
     svg.append('text')
-    .attr('x', (width / 2) + margin.x)
-    .attr('y', (margin.y * 2))
-    .attr('transform',`translate(0,${height - (margin.y / 4) })`)
-    .attr('class','title')
-    .text('x axis title')
+        .attr('x', (width / 2) + margin.x)
+        .attr('y', (margin.y * 2))
+        .attr('transform', `translate(0,${height - (margin.y / 4)})`)
+        .attr('class', 'title')
+        .text('x axis title')
     //End x axis title
 
     //y Axis Title
     svg.append('text')
-    .attr('class', 'title')
-    .attr('x',-(height/2) - margin.y)
-    .attr('y', margin.x / 2.4)
-    .attr('transform', 'rotate(-90)')
-    .text('y axis title')
+        .attr('class', 'title')
+        .attr('x', -(height / 2) - margin.y)
+        .attr('y', margin.x / 2.4)
+        .attr('transform', 'rotate(-90)')
+        .text('y axis title')
     //End y Axis Title
 
     const line = d3.line()
-    .x(function(d,i){ return xScale(d.language) })
-    .y(function(d,i){ return yScale(d.value) })
-    .curve(d3.curveMonotoneX)
+        .x(function (d, i) { return xScale(d.language) })
+        .y(function (d, i) { return yScale(d.value) })
+        .curve(d3.curveMonotoneX)
 
     chart.append('path')
-    .datum(datas)
-    .attr('class','line')
-    .attr('d',line)
+        .datum(datas)
+        .attr('class', 'line')
+        .attr('d', line)
 
 }
 drawPieChart()
-function drawPieChart(){
+function drawPieChart() {
     const good_datas = []
     let total = 0
-    for(let i = 0; i < datas.length; i++){
+    for (let i = 0; i < datas.length; i++) {
         total = +datas[i].value + +total
     }
-    for(let i = 0; i < datas.length; i++){
+    for (let i = 0; i < datas.length; i++) {
         good_datas.push(
             {
-                value : datas[i].value/total, 
-                color : "#AA" + Math.ceil(Math.random() * 10000),
-                title : datas[i].language
+                value: datas[i].value / total,
+                color: "#AA" + Math.ceil(Math.random() * 10000),
+                title: datas[i].language
             })
     }
 
     const area = document.querySelector('#pie')
-    const   margin  = {y : 40, x : 60},
-    width   = area.offsetWidth - (2 * margin.x),
-    height  = area.offsetHeight - (2 * margin.y);
+    const margin = { y: 40, x: 60 },
+        width = area.offsetWidth - (2 * margin.x),
+        height = area.offsetHeight - (2 * margin.y);
     const svg = d3.select(area).append("svg")
-    svg.attr("width",width + (2 * margin.x))
-    .attr("height",height + (2 * margin.y))
-    .attr('id','svg-pie')
+    svg.attr("width", width + (2 * margin.x))
+        .attr("height", height + (2 * margin.y))
+        .attr('id', 'svg-pie')
     const chart = svg.append('g')
-    .attr('transform',`translate(${(width + (2 * margin.x)) / 4},${(height + (2 * margin.y)) / 2})`)
+        .attr('transform', `translate(${(width + (2 * margin.x)) / 4},${(height + (2 * margin.y)) / 2})`)
     const legend = svg.append('g')
-    .attr('transform',`translate(${(width + (2 * margin.x)) / 2},${margin.y})`)
+        .attr('transform', `translate(${(width + (2 * margin.x)) / 2},${margin.y})`)
 
     //Top Title
     svg.append('text')
-    .attr('x', (width / 2) + margin.x)
-    .attr('y', margin.y / 2)
-    .attr('class','title')
-    .attr('text-anchor','middle')
-    .text('Pie chart')
+        .attr('x', (width / 2) + margin.x)
+        .attr('y', margin.y / 2)
+        .attr('class', 'title')
+        .attr('text-anchor', 'middle')
+        .text('Pie chart')
     //End Top Title
 
     const radius = (width < height) ? width / 2 : height / 2;
 
     const arc = d3.arc()
-    .innerRadius(radius)
-    .outerRadius(0)
+        .innerRadius(radius)
+        .outerRadius(0)
 
-    const pie = d3.pie().value(function(d,i){ return d.value })
+    const pie = d3.pie().value(function (d, i) { return d.value })
 
     chart.selectAll('arc')
-    .data(pie(good_datas))
-    .enter()
+        .data(pie(good_datas))
+        .enter()
         .append('path')
-        .attr('d',arc)
-        .style('fill',function(d,i){ return good_datas[i].color })
+        .attr('d', arc)
+        .style('fill', function (d, i) { return good_datas[i].color })
     legend.selectAll('circle')
-    .data(good_datas)
-    .enter()
+        .data(good_datas)
+        .enter()
         .append('circle')
-        .attr('r',5)
-        .attr('cx',20)
-        .attr('cy', function(d,i){ return i*20 })
-        .attr('fill',function(d,i){ return d.color })
+        .attr('r', 5)
+        .attr('cx', 20)
+        .attr('cy', function (d, i) { return i * 20 })
+        .attr('fill', function (d, i) { return d.color })
     legend.selectAll('text')
         .data(good_datas)
         .enter()
         .append('text')
-            .attr('class','title')
-            .attr('x',50)
-            .attr('y', function(d,i){ return 5 + (i * 20) })
-            .text(function(d,i){ return d.title })
+        .attr('class', 'title')
+        .attr('x', 50)
+        .attr('y', function (d, i) { return 5 + (i * 20) })
+        .text(function (d, i) { return d.title })
 }
+*/
---
+++ map.js
@@ -0,0 +1,310 @@
+// Assign dimensions for map container
+var width = 800,
+	height = 600;
+
+// Field refernce to csv column
+var field = "total_votes"
+
+//Number formatting
+//
+//Create a function that take a number, adds commas for thousands,
+//and removes decimal values, e.g. 1234567.89 --> 1,234,567
+//
+var valueFormat = d3.format(",");
+
+// Get and prepare the Mustache template; parsing it speeds up future uses
+var template = d3.select('#template').html();
+Mustache.parse(template);
+
+// Logic to handle hover event when its firedup
+var hoveron = function (d) {
+	console.log('d', d, 'event', window.event);
+	var div = document.getElementById('tooltip');
+	div.style.left = window.event.pageX + 'px';
+	div.style.top = window.event.pageY + 'px';
+
+
+	//Fill yellow to highlight
+	d3.select(this)
+		.style("fill", "yellow");
+
+	//Show the tooltip
+	d3.select("#tooltip")
+		.style("opacity", 1);
+
+	//Populate name in tooltip
+	d3.select("#tooltip .name")
+		.text(d.properties.dist);
+
+	//Populate value in tooltip
+	d3.select("#tooltip .value")
+		.text(valueFormat(d.properties.field) + "%");
+}
+
+var hoverout = function (d) {
+
+	//Restore original choropleth fill
+	d3.select(this)
+		.style("fill", function (d) {
+			var value = d.properties.field;
+			if (value) {
+
+				var id = getIdOfFeature(d);
+				var d = dataById[id];
+
+				var winner = determineWinner(d);
+				return generateColor(winner);
+			} else {
+				return "#ccc";
+			}
+		});
+
+	//Hide the tooltip
+	d3.select("#tooltip")
+		.style("opacity", 0);
+
+}
+
+
+// Create SVG inside map container and assign dimensions
+var svg = d3.select("#map").append('svg')
+	.attr("width", width)
+	.attr("height", height);
+
+// Define a geographical projection
+// Also, set initial zoom to show the features
+var projection = d3.geo.mercator()
+	.scale(1);
+
+// Prepare a path object and apply the projection to it
+var path = d3.geo.path()
+	.projection(projection);
+
+// We prepare an object to later have easier access to the data.
+var dataById = d3.map();
+
+//Define quantize scale to sort data values into buckets of color
+//Colors by Cynthia Brewer (colorbrewer2.org), 9-class YlGnBu
+
+var color = d3.scale.quantize()
+	/*.range(d3.range(9),map(function(i) { return 'q' + i + '-9';}));*/
+	/*.range(["#a50026",
+		"#d73027",
+		"#f46d43",
+		"#fdae61",
+		"#fee08b",
+		"#ffffbf",
+		"#d9ef8b",
+		"#a6d96a",
+		"#66bd63",
+		"#1a9850",
+		"#006837"]);*/
+		.range(["#a50026",
+		"#fee08b",
+		"#f46d43",
+		"#66bd63",
+		"#1a9850",
+		"#a6d96a"]);
+
+// Load in coverage score data
+//d3.csv("/map_data/coverage16_v3.csv", function (data) {
+d3.csv("data/presidential_results.csv", function (data) {
+
+
+	//Set input domain for color scale
+	color.domain([
+		d3.min(data, function (d) { return +d[field]; }),
+		d3.max(data, function (d) { return +d[field]; })
+
+	]);
+
+	// This maps the data of the CSV so it can be easily accessed by
+	// the ID of the district, for example: dataById[2196]
+	dataById = d3.nest()
+		.key(function (d) { return d.id; })
+		.rollup(function (d) { return d[0]; })
+		.map(data);
+
+	// Load features from GeoJSON
+	d3.json('data/ug_districts3.geojson', function (error, json) {
+
+
+		// Get the scale and center parameters from the features.
+		var scaleCenter = calculateScaleCenter(json);
+
+		// Apply scale, center and translate parameters.
+		projection.scale(scaleCenter.scale)
+			.center(scaleCenter.center)
+			.translate([width / 2, height / 2]);
+
+		// Merge the coverage data amd GeoJSON into a single array
+		// Also loop through once for each coverage score data value
+
+		for (var i = 0; i < data.length; i++) {
+
+			// Grab district name
+			var dataDistrict = data[i].district;
+
+			//Grab data value, and convert from string to float
+			var dataValue = +data[i][field];
+
+			//Find the corresponding district inside GeoJSON
+			for (var j = 0; j < json.features.length; j++) {
+
+				// Check the district reference in json
+				var jsonDistrict = json.features[j].properties.dist;
+
+				if (dataDistrict == jsonDistrict) {
+
+					//Copy the data value into the GeoJSON
+					json.features[j].properties.field = dataValue;
+
+					//Stop looking through JSON
+					break;
+				}
+			}
+		}
+
+		// Add a <g> element to the SVG element and give a class to style later
+		svg.append('g')
+			.attr('class', 'features')
+        //Top Title
+        svg.append('text')
+            //.attr('x', (width / 2))
+            //.attr('y', height / 2)
+            .attr('class', 'title')
+            .attr('text-anchor', 'middle')
+            .text('% Voter turnout Vs Invalid votes')
+        //End Top Title
+
+		// Bind data and create one path per GeoJSON feature
+		svg.selectAll("path")
+			.data(json.features)
+			.enter()
+			.append("path")
+			.attr("d", path)
+			//.on("mouseover", hoveron)
+			.on("mouseover", showDetails)
+			.on("mouseout", hoverout)
+			//.on('click', showDetails)
+			.style("cursor", "pointer")
+			.style("stroke", "#777")
+			.style("fill", function (d) {
+
+				// Get data value
+
+				var value = d.properties.field;
+
+				if (value) {
+					// If value exists ...
+					var id = getIdOfFeature(d);
+					var d = dataById[id];
+					
+					var winner = determineWinner(d);
+					console.log(winner);
+					return generateColor(winner);
+					//color(value);
+				} else {
+					// If value is undefines ...
+					return "#000";
+				}
+			});
+
+	}); // End d3.json
+}); // End d3.csv
+
+function determineWinner(data)
+{
+	//determine candidates votes
+	var candidates = {
+		museveni : data.museveni,
+		kyagulanyi : data.kyagulanyi,
+		mao : data.mao,
+		amuriat : data.amuriat,
+		tumukunde : data.tumukunde,
+        mwesigye: data.mwesigye,
+        muntu: data.muntu,
+        mayambala: data.mayambala,
+        kalembe: data.kalembe,
+        katumba: data.katumba,
+        kabuleta: data.kabuleta
+       // museveni", "mao", "tumukunde", "mwesigye", "muntu", "mayambala", "kyagulanyi", "kalembe", "katumba", "amuriat", "kabuleta
+	};
+
+	//Object.keys(candidates).reduce(function(a, b){ return candidates[a] > candidates[b] ? a : b });
+	winner = Object.entries(candidates).sort((x, y) => y[1] - x[1])[0];
+	return winner[0];
+}
+
+function generateColor(value)
+{
+	// Create data
+	var data = ["museveni", "mao", "tumukunde", "mwesigye", "muntu", "mayambala", "kyagulanyi", "kalembe", "katumba", "amuriat", "kabuleta"]
+
+	// Option 1: provide color names:
+	//var myColor = d3.scaleOrdinal().domain(data)
+	var myColor = d3.scale.ordinal().domain(data)
+	.range(["gold", "green", "white", "brown", "grey", "cream", "red", "brown", "slateblue", "blue", "orange"]);
+	
+	console.log(myColor(value));
+	
+	return myColor(value);
+}
+
+// NEW: function to dynamically calculate the scale factror and center
+
+function calculateScaleCenter(features) {
+	// Get the bounding box of the paths (in pixels) and calculate a scale factor based on box and map size
+	var bbox_path = path.bounds(features),
+		scale = 0.95 / Math.max(
+			(bbox_path[1][0] - bbox_path[0][0]) / width,
+			(bbox_path[1][1] - bbox_path[0][1]) / height
+		);
+
+	// Get the bounding box of the features (in map units) and use it to calculate the center of the features.
+	var bbox_feature = d3.geo.bounds(features),
+		center = [
+			(bbox_feature[1][0] + bbox_feature[0][0]) / 2,
+			(bbox_feature[1][1] + bbox_feature[0][1]) / 2];
+
+	return {
+		'scale': scale,
+		'center': center
+	};
+}
+
+// NEW: function to show details on click
+function showDetails(f) {
+
+	console.log('d', d, 'event', window.event);
+	var div = document.getElementById('tooltip');
+	div.style.left = window.event.pageX + 'px';
+	div.style.top = window.event.pageY + 'px';
+
+	//Fill yellow to highlight
+	d3.select(this)
+		.style("fill", "white");
+
+	//Show the tooltip
+	d3.select("#tooltip")
+		.style("opacity", 1);
+
+	var id = getIdOfFeature(f); //Get the ID of the feature
+	var d = dataById[id]; // Use the ID to get the data entry
+	//console.log(d) //testing
+	var detailsHtml = Mustache.render(template, d); // Render the Mustace template with the data object
+
+	console.log(f.properties);
+	//alert(d.museveni);
+	//Hide the initial container.
+	d3.select('#initial').classed('hidden', true);
+
+	// Put the HTML output in the details container and show (unhide) it.
+	d3.select('#details').html(detailsHtml);
+	d3.select('#details').classed('hidden', false);
+}
+
+// NEW: Defining getIdOfFeature
+function getIdOfFeature(f) {
+	return f.properties.idug;
+}
